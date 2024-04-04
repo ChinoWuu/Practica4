5702497f4d2666e8e3689e281ae65a3c34282970
JBarcenas <jesus.barcenas@uabc.edu.mx>
2024-04-04 10:28:26 -0700

    agregamos 2 ejemplos y un ejercicio 4/4/2024

---
+++ ejemplo2.py
@@ -0,0 +1,7 @@
+num1=int(input("ingrese primer valor:"))
+num2=int(input("ingrese segundo valor:"))
+suma=num1+num2
+producto=num1*num2
+print("la suma de los valores es:",suma)
+
+print("el producto de los valores es:",producto)
---
+++ ejercicio1.1.py
@@ -0,0 +1,4 @@
+pre=int(input("ingrese el valor del producto:"))
+cant=int(input("Cuantos productos son:"))
+importe=pre*cant
+print("la cuenta es:",importe)
